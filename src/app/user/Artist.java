7f52801231eccf1e9c9edf9927796a25a2b95fc5
laurentiuracovcen <118909961+laurentiuracovcen@users.noreply.github.com>
2023-12-11 00:03:42 +0200

    Implemented SwitchConnectionStatus and added new User types

---
+++ src/app/user/Artist.java
@@ -0,0 +1,31 @@
+package app.user;
+
+import app.audio.Collections.AudioCollection;
+import app.audio.Collections.Playlist;
+import app.audio.Collections.PlaylistOutput;
+import app.audio.Files.AudioFile;
+import app.audio.Files.Song;
+import app.audio.LibraryEntry;
+import app.player.Player;
+import app.player.PlayerStats;
+import app.searchBar.Filters;
+import app.searchBar.SearchBar;
+import app.utils.Enums;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Artist extends User {
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param username the username
+     * @param age      the age
+     * @param city     the city
+     */
+    public Artist(String username, int age, String city) {
+        super(username, age, city);
+    }
+}
